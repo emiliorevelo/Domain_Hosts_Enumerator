fd38d636ba30d9367c7bbb4b125385d9e883e796
emiliorevelo <emilio_revelo@protonmail.com>
2019-06-06 19:26:55 -0500

    Add files via upload

---
+++ DHosts_enum.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Emilio Revelo
+
+#If you want to obtain just the hostnames use:
+
+"cat [Input_file] | cut -d ' ' -f3 | cut -d '.' -f1 > [Input_file]"
+
+i=0
+j=0
+
+for i in {1..255}; do
+	
+	for j in {1.255};do
+	
+	nslookup 192.168.$i.$j | grep name
+	j=$[$j+1]
+	
+	done
+i=$[$i+1]
+done
+
+
